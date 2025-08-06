166e223557240ecef978832a2a14cc4dcf4b2bea
carlvincet <carlvincet@student.unimelb.edu.au>
2025-08-06 17:15:53 +1000

    Added themes based on draft logo colourway and typography

---
+++ lib/app/auth/index.dart
@@ -0,0 +1,5 @@
+export 'forgot_password.dart';
+export 'login.dart';
+export 'restore_password.dart';
+export 'signup.dart';
+export 'verify_email.dart';
---
+++ lib/theme/app_pallete.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppPallete {
+  // Common Colors
+  static const Color error = Color(0xFFFF5963);
+  static const Color hyperlink = Color(0xFF008AFF);
+  static const Color cursorLight = Color(0xFFBFBFBF);
+
+  // Dark Theme Colors
+  static const Color darkPrimary = Color(0xFF5A7360);
+  static const Color darkSecondary = Color(0xFF46594B);
+  static const Color darkTertiary = Color(0xFF24261C);
+  static const Color darkQuaternary = Color(0xFF848C68);
+  static const Color darkQuinary = Color(0xFFF2E3D5);
+
+  static const Color primaryText = darkQuinary;
+  static const Color secondaryText = darkQuaternary;
+  static const Color alternateText = darkSecondary;
+  static const Color infoTextLight = darkQuaternary;
+  static const Color infoTextDark = darkSecondary;
+
+  static const Color primaryBackground = darkTertiary;
+  static const Color secondaryBackground = darkSecondary;
+
+  // Light Theme Colors
+  static const Color lightPrimary = Color(0xFF5A7360);
+  static const Color lightSecondary = Color(0xFF46594B);
+  static const Color lightTertiary = Color(0xFF848C68);
+  static const Color lightQuaternary = Color(0xFFA3A697);
+  static const Color lightQuinary = Color(0xFFF2E3D5);
+
+  static const Color lightPrimaryText = lightQuinary;
+  static const Color lightSecondaryText = lightQuaternary;
+  static const Color lightAlternateText = lightSecondary;
+  static const Color lightInfoTextLight = lightQuaternary;
+  static const Color lightInfoTextDark = lightSecondary;
+
+  static const Color lightPrimaryBackground = lightQuinary;
+  static const Color lightSecondaryBackground = lightPrimary;
+}
