e799ccb94fcbcf830b1820f918a6bb461cac79be
carlvincet <carlvincet@student.unimelb.edu.au>
2025-08-09 21:40:29 +1000

    removed old builder in app_router.dart

--- lib/core/app_router.dart
+++ lib/core/app_router.dart
@@ -9,52 +9,42 @@ final GoRouter router = GoRouter (
   routes: [
     GoRoute(
       path: '/splash-screen',
-      builder: (context, state) => const SplashScreen(),
       pageBuilder: (context, state) {
         return NoTransitionPage(child: const SplashScreen());
       },
     ),
     GoRoute(
       path: '/login',
-      builder: (context, state) => const LoginPage(),
       pageBuilder: (context, state) {
         return NoTransitionPage(child: const LoginPage());
       },
     ),
     GoRoute(
       path: '/dashboard',
-      builder: (context, state) => const DashboardPage(),
       pageBuilder: (context, state) {
         return NoTransitionPage(child: const DashboardPage());
       },
     ),
     GoRoute(
       path: '/signup',
-      builder: (context, state) => const SignUpPage(),
       pageBuilder: (context, state) {
         return NoTransitionPage(child: const SignUpPage());
       },
     ),
     GoRoute(
       path: '/forgot-password',
-      builder: (context, state) => const ForgotPasswordPage(),
       pageBuilder: (context, state) {
         return NoTransitionPage(child: const ForgotPasswordPage());
       },
     ),
     GoRoute(
       path: '/restore-password',
-      builder: (context, state) => const RestorePasswordPage(),
       pageBuilder: (context, state) {
         return NoTransitionPage(child: const RestorePasswordPage());
       },
     ),
     GoRoute(
       path: '/verify-email',
-      builder: (context, state) {
-        final email = state.uri.queryParameters['email'] ?? '';
-        return VerfiyEmailPage(email: email);
-      },
       pageBuilder: (context, state) {
         final email = state.uri.queryParameters['email'] ?? '';
         return NoTransitionPage(child: VerfiyEmailPage(email: email));
